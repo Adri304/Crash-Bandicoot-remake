c60e0b5080023dd493e8a5ac37eaf4acd6ffdf5e
GabrielAGES <gaguilar004@soyudemedellin.edu.co>
2026-05-04 23:33:00 -0500

    Spawn funcional (falta mejorar)
    Funciona y genera objetos pero falta subir la altura de la generación

---
+++ ADG/Assets/_Main/All_Scrpits/SpawnEnemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject objetoPrefab;
+    public float tiempoEntreSpawns = 2f;
+    public Vector2 rangoX = new Vector2(-2, 2);
+    public Vector2 rangoZ = new Vector2(-2, 2);
+
+    void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            SpawnObjeto();
+            yield return new WaitForSeconds(tiempoEntreSpawns);
+        }
+    }
+
+    void SpawnObjeto()
+    {
+        float x = Random.Range(rangoX.x, rangoX.y);
+        float z = Random.Range(rangoZ.x, rangoZ.y);
+
+        Vector3 posicion = new Vector3(x, 0.5f, z); // altura sobre el plano
+
+        Instantiate(objetoPrefab, posicion, Quaternion.identity);
+    }
+}
