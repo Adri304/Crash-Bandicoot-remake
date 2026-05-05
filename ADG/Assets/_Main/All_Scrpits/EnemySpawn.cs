03a272fc17aaf6dacdc20d3de90c226cfe5810b2
gaguilar004U <gaguilar004@soyudemedellin.edu.co>
2026-05-05 12:25:45 -0500

    Correccion de la altura de generacion del enemigo
    Se corrige en el script la atura de generacion para que quede sobre el plano y no en la mitad de este

--- ADG/Assets/_Main/All_Scrpits/EnemySpawn.cs
+++ ADG/Assets/_Main/All_Scrpits/EnemySpawn.cs
@@ -27,7 +27,7 @@ public class Spawner : MonoBehaviour
         float x = Random.Range(rangoX.x, rangoX.y);
         float z = Random.Range(rangoZ.x, rangoZ.y);
 
-        Vector3 posicion = new Vector3(x, 0.5f, z); // altura sobre el plano
+        Vector3 posicion = new Vector3(x, 1.01f, z); // altura sobre el plano
 
         Instantiate(objetoPrefab, posicion, Quaternion.identity);
     }
